f9e7a216e3ebaf5b68145d354ee6ddbfe4a0b99d
Keshava Munegowda <keshava.gowda@gmail.com>
2021-02-16 11:48:22 +0530

    Draft implementation of grpc secure hello world with GRPC.
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

---
+++ cpp/helper.cc
@@ -0,0 +1,66 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "helper.h"
+#include <string>
+#include <fstream>
+
+
+bool readFile(const std::string &filepath,std::string &buffer){
+    std::ifstream fin(filepath.c_str());
+    if (fin.good()) {
+        std::cout<< "file :"+filepath+" found!" <<std::endl;
+        getline(fin, buffer, (char) fin.eof());
+        fin.close();
+        return true;
+    }
+    std::cout<< "file :"+filepath+" not found!" <<std::endl;
+    return false;
+}
+
+void readCertParams(int argc, char** argv, SecureParams &out) {
+    for (int i=0; i < argc ; i++) {
+        if (strcasecmp(argv[i],"-ca")==0) {
+            out.caFile = argv[i+1];
+        } else if (strcasecmp(argv[i],"-cert")==0) {
+            out.certFile =  argv[i+1];
+        } else if (strcasecmp(argv[i],"-key")==0) {
+            out.keyFile =  argv[i+1];
+        }
+    }
+}
+
+bool parseSecureCredentials(SecureParams &params, SecureCredentials &out) {
+    if (params.caFile.empty()) {
+        out.ca = "";
+    } else {
+        readFile(params.caFile, out.ca);
+    }
+    if (params.certFile.empty()) {
+        std::cout << "Supply certificate file with option -cert " << std::endl;
+        return false;
+    }
+    if (params.keyFile.empty()) {
+        std::cout << "Supply key file with option -key " << std::endl;
+        return false;
+    }
+    if (!readFile(params.certFile, out.cert))   {
+        return false;
+    }
+    return readFile(params.keyFile, out.key);
+}
---
+++ cpp/secure_greeter_server.cc
@@ -0,0 +1,91 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include "helper.h"
+
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+// Logic and data behind the server's behavior.
+class SecureGreeterServiceImpl final : public Greeter::Service {
+  Status SayHello(ServerContext* context, const HelloRequest* request,
+                  HelloReply* reply) override {
+    std::string prefix("Hello ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
+};
+
+void RunServer(int argc, char** argv) {
+  std::string server_address("0.0.0.0:50051");
+  SecureGreeterServiceImpl service;
+  SecureParams params;
+  SecureCredentials certs;
+  readCertParams(argc, argv, params);
+  if (!parseSecureCredentials(params, certs)) {
+      std::cout << "parseSecureCredentials failed " << std::endl;
+      return;
+  }
+  grpc::SslServerCredentialsOptions ssl_opts;
+  std::shared_ptr<grpc::ServerCredentials> creds;
+  grpc::SslServerCredentialsOptions::PemKeyCertPair pkcp ={certs.key, certs.cert};
+  ssl_opts.pem_root_certs=certs.ca;
+  ssl_opts.pem_key_cert_pairs.push_back(pkcp);
+  creds = grpc::SslServerCredentials(ssl_opts);
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, creds);
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Secure Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer(argc, argv);
+
+  return 0;
+}
